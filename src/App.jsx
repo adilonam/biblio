82bc8d962e3f38e623153570715074633c11f44c
Adil ABBADI <adil.abbadi@ifazs.gov.ma>
2023-05-17 22:37:43 +0100

    navbar and home init

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useLayoutEffect, useRef, useEffect} from 'react'
+// Import all of Bootstrap's CSS
+import './App.scss'
+// Import our custom CSS
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import { NavBar } from './navbar/components/NavBar'
+//prime react css
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";  //icons
+// import { auth } from './firebase';
+// import {  onAuthStateChanged } from "firebase/auth";
+
+
+
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Home } from './pages/home/components/Home';
+// import { SignUp } from './pages/signup/components/SignUp';
+// import { Copyright } from './copyright/components/Copyright';
+
+
+
+// import { SignIn } from './pages/signin/components/SignIn';
+// import { UserInfo } from './userinfo/components/UserInfo';
+// import { Admin } from './pages/admin/components/Admin';
+// import { Footer } from './footer/components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const urls = [
+    '/',
+    '/inscrire',
+    '/connecter',
+    '/admin',
+    '/sponsor',
+    '/media',
+    '/contact'
+  ]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  
+  const [user, setUser] = useState(null)
+
+  // onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     // User is signed in, see docs for a list of available properties
+  //     // https://firebase.google.com/docs/reference/js/firebase.User
+  //     const uid = user.uid;
+  //     setUser(user)
+  //   } else {
+  //     setUser(null)
+  //   }
+  // });
+
+  return (<>
+
+    <NavBar urls={urls} user={user} ></NavBar>
+{/* 
+{ user && 
+   <UserInfo user={user}></UserInfo>
+
+    } */}
+    <Routes>
+      
+<Route path={urls[0]} element={<Home urls={urls}></Home>}></Route>
+{/* <Route path={urls[1]} element={<SignUp></SignUp>}></Route>
+<Route path={urls[2]} element={<SignIn></SignIn>}></Route>
+<Route path={urls[3]} element={<Admin user={user} ></Admin>}></Route> */}
+   
+   
+    </Routes>
+   
+
+
+
+  </>
+ 
   )
 }
 
